66449c8d3579cae7eade7c55d269f39d7dfe89d5
Kartik-Ranaut <ranautk6@gmail.com>
2025-04-17 11:47:48 +0530

    resolved the render deployment issue

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const UserPet = require("../Models/UserAsPet");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
